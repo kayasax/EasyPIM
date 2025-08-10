cd9e6b97a9f61b984cfbef94400d6936a07c03c2
Loïc MICHEL <loic.michel@gmail.com>
2025-08-10 20:22:02 +0200

    refactor: move internal Normalize-IsoDuration helper out of root module file

--- EasyPIM/EasyPIM.psm1
+++ EasyPIM/EasyPIM.psm1
@@ -9,3 +9,5 @@ foreach ($file in Get-ChildItem -Path "$PSScriptRoot/functions" -Filter *.ps1 -R
 foreach ($file in Get-ChildItem -Path "$PSScriptRoot/internal/scripts" -Filter *.ps1 -Recurse) {
     . $file.FullName
 }
+
+## Note: All internal helper functions (including Normalize-IsoDuration) are loaded from internal/functions/*.
